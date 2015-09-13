885347d0e0b16b8e9f44adb7a570f7d3ddf90f4a
nzonbi <nzonbi@gmail.com>
2015-09-13 18:11:41 -0430

    Add jasmine tests specs for api ops

--- spec/microbiSpec.js
+++ spec/microbiSpec.js
@@ -47,7 +47,7 @@ describe( "Microbi", function() {
           done()
         })
       }).on( "error", function( e ) {
-        fail( "Server error" )
+        fail( "Request Error" )
       });
 
     });
@@ -66,7 +66,7 @@ describe( "Microbi", function() {
           done()
         })
       }).on( "error", function( e ) {
-        fail( "Server error" )
+        fail( "Request Error" )
       });
 
     });
@@ -77,9 +77,11 @@ describe( "Microbi", function() {
 
   describe( "Api server", function() {
     var helloUrl = "http://localhost:55555/test/hello"
-    var queryParamsUrl = "http://localhost:55555/stuff/items?a=1&b=2"
     var staticFileUrl = "http://localhost:55555/spec/support/test_data.txt"
-    var notFounfFileUrl = "http://localhost:55555/spec/support/no_file.txt"
+    var notFoundFileUrl = "http://localhost:55555/spec/support/no_file.txt"
+    var getInfoUrl = "http://localhost:55555/test/info"
+    var pathParamsUrl = "http://localhost:55555/test/pathParams/123/abc"
+    var mimeAltUrl = "http://localhost:55555/test/mimeAlt"
 
     beforeAll( function() {
       var api = {}
@@ -95,8 +97,25 @@ describe( "Microbi", function() {
       api.test.hello.POST = function( info ) {
         return "hello"
       }
-
-
+      api.test.info = {}
+      api.test.info.GET = function( info ) {
+        return JSON.stringify( info )
+      }
+      api.test.mirrorBody = {}
+      api.test.mirrorBody.POST = function( info ) {
+        return info.body
+      }
+      api.test.pathParams = {}
+      api.test.pathParams.$x = {}
+      api.test.pathParams.$x.$x = {}
+      api.test.pathParams.$x.$x.GET = function( info ) {
+        return JSON.stringify( info )
+      }
+      api.test.mimeAlt = {}
+      api.test.mimeAlt["GET:mime"] = "html"
+      api.test.mimeAlt.GET = function( info ) {
+        return "<div>some html</div>"
+      }
     });
 
 
@@ -115,8 +134,29 @@ describe( "Microbi", function() {
           done()
         })
       }).on( "error", function( e ) {
-        fail( "Server error" )
+        fail( "Request Error" )
       });
+    });
+
+
+
+    it( "should respond 404 when a file is not found", function( done ) {
+
+      http.get( notFoundFileUrl, function( res ) {
+        var body = ""
+        res.on( "data", function ( chunk ) {
+          body += chunk
+        })
+        res.on( "end", function() {
+          expect( res.statusCode ).toEqual( 404 );
+          expect( res.headers["content-type"] ).toEqual( "text/plain" );
+          expect( body ).toEqual( "404 Not found." );
+          done()
+        })
+      }).on( "error", function( e ) {
+        fail( "Request Error" )
+      });
+
     });
 
 
@@ -129,10 +169,10 @@ describe( "Microbi", function() {
         path: "/test/hello",
         method: "POST",
         headers: {
-          'Content-Type': "text/plain",
-          'Content-Length': 0
+          "Content-Type": "text/plain",
+          "Content-Length": 0
         }
-      }, function(res) {
+      }, function( res ) {
         var body = ""
         res.on( "data", function ( chunk ) {
           body += chunk
@@ -147,7 +187,7 @@ describe( "Microbi", function() {
           fail( "Server error" )
         })
       })
-      req.on('error', function(e) {
+      req.on("error", function(e) {
         fail( "Request error" )
       });
       req.end()
@@ -167,11 +207,11 @@ describe( "Microbi", function() {
         res.on( "end", function() {
           expect( res.statusCode ).toEqual( 200 );
           expect( res.headers["content-type"] ).toEqual( "text/plain" );
-          expect( body ).toEqual("abc\n123\n" );
+          expect( body ).toEqual( "abc\n123\n" );
           done()
         })
       }).on( "error", function( e ) {
-        fail( "Server error" )
+        fail( "Request Error" )
       });
 
     });
@@ -182,7 +222,7 @@ describe( "Microbi", function() {
       "then look for a static file, and if none is found neither, " +
       "then it should respond 404 not found", function( done ) {
 
-      http.get( notFounfFileUrl, function( res ) {
+      http.get( notFoundFileUrl, function( res ) {
         var body = ""
         res.on( "data", function ( chunk ) {
           body += chunk
@@ -194,7 +234,136 @@ describe( "Microbi", function() {
           done()
         })
       }).on( "error", function( e ) {
-        fail( "Server error" )
+        fail( "Request Error" )
+      });
+
+    });
+
+
+    it( "should provide request info to api methods", function( done ) {
+
+      http.get( getInfoUrl, function( res ) {
+        var body = ""
+        res.on( "data", function ( chunk ) {
+          body += chunk
+        });
+        res.on( "end", function() {
+          expect( res.statusCode ).toEqual( 200 );
+          expect( res.headers["content-type"] ).toEqual( "text/plain" );
+          var json = JSON.parse(body)
+          expect( json.method ).toEqual( "GET" );
+          expect( json.pathname ).toEqual( "/test/info" );
+          expect( json.queryParams ).toEqual( {} );
+          expect( json.pathParams ).toEqual( [] );
+          expect( json.body ).toEqual( "" );
+          done()
+        })
+      }).on( "error", function( e ) {
+        fail( "Request Error" )
+      });
+
+    });
+
+
+
+    it( "should provide the request body to api methods", function( done ) {
+
+      var message = "This is the message\nto send"
+      var req = http.request({
+        hostname: "localhost",
+        port: 55555,
+        path: "/test/mirrorBody",
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+          "Content-Length": message.length
+        }
+      }, function( res ) {
+        var body = ""
+        res.on( "data", function ( chunk ) {
+          body += chunk
+        })
+        res.on( "end", function() {
+          expect( res.statusCode ).toEqual( 200 );
+          expect( res.headers["content-type"] ).toEqual( "text/plain" );
+          expect( body ).toEqual( message );
+          done()
+        })
+        res.on( "error", function ( e ) {
+          fail( "Server error" )
+        })
+      })
+      req.on("error", function(e) {
+        fail( "Request error" )
+      });
+      req.write( message )
+      req.end()
+
+    });
+
+
+
+    it( "should provide query parameters to api methods", function( done ) {
+
+      http.get( getInfoUrl + "?a=1&b=2", function( res ) {
+        var body = ""
+        res.on( "data", function ( chunk ) {
+          body += chunk
+        });
+        res.on( "end", function() {
+          expect( res.statusCode ).toEqual( 200 );
+          expect( res.headers["content-type"] ).toEqual( "text/plain" );
+          var json = JSON.parse(body)
+          expect( json.queryParams.a ).toEqual( "1" );
+          expect( json.queryParams.b ).toEqual( "2" );
+          done()
+        })
+      }).on( "error", function( e ) {
+        fail( "Request Error" )
+      });
+
+    });
+
+
+
+    it( "should provide path parameters to api methods", function( done ) {
+
+      http.get( pathParamsUrl, function( res ) {
+        var body = ""
+        res.on( "data", function ( chunk ) {
+          body += chunk
+        });
+        res.on( "end", function() {
+          expect( res.statusCode ).toEqual( 200 );
+          expect( res.headers["content-type"] ).toEqual( "text/plain" );
+          var json = JSON.parse(body)
+          expect( json.pathParams[0] ).toEqual( "123" );
+          expect( json.pathParams[1] ).toEqual( "abc" );
+          done()
+        })
+      }).on( "error", function( e ) {
+        fail( "Request Error" )
+      });
+
+    });
+
+
+
+    it( "should allow to specify the mime type of api ops", function( done ) {
+
+      http.get( mimeAltUrl, function( res ) {
+        var body = ""
+        res.on( "data", function ( chunk ) {
+          body += chunk
+        });
+        res.on( "end", function() {
+          expect( res.statusCode ).toEqual( 200 );
+          expect( res.headers["content-type"] ).toEqual( "text/html" );
+          expect( body ).toEqual( "<div>some html</div>" );
+          done()
+        })
+      }).on( "error", function( e ) {
+        fail( "Request Error" )
       });
 
     });
@@ -202,6 +371,8 @@ describe( "Microbi", function() {
 
 
   })
+
+
 
 
 })
